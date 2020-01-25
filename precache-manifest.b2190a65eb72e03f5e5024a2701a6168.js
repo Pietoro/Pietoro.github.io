f94248b06898ae9f86b4de9e4709bc84e33ff73e
Ewelina Kania <ewelina.kania93@gmail.com>
2020-01-25 19:07:13 +0100

    Release version 0.1

---
+++ precache-manifest.b2190a65eb72e03f5e5024a2701a6168.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917b77994c910e0ff45b352fbfcd6efe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3880ede26038d9046c62",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0a2028a4d87db4cb3693",
+    "url": "/static/js/2.4d7c1c5a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.4d7c1c5a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3880ede26038d9046c62",
+    "url": "/static/js/main.741055d1.chunk.js"
+  },
+  {
+    "revision": "f483d1f7cd20d8e3322f",
+    "url": "/static/js/runtime-main.70302f69.js"
+  }
+]);
